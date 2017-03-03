8fb589d136862b1198df79f4bbbedf6c0450d7a8
jmd338 <jmd338@georgetown.edu>
2017-03-03 16:30:13 -0500

    Outline

--- ChatterServer.java
+++ ChatterServer.java
@@ -1,10 +1,16 @@
 package ChattyRepo;
 
 import java.net.*;
+import java.util.*;
 import java.io.*;
 //TEST FROM KAZ
 public class ChatterServer
 {
+	
+	//this object stores the addresses of every ServerListens class
+	//that gets instantiated
+	LinkedList chatting;
+	
 	
 	//this function opens the socket and waits for the call
 	public void answerThePhone()
@@ -24,6 +30,12 @@ public class ChatterServer
 	//must go to every client
 	//opens a Writer to write to ClientListens
 	public void tellOthers()
+	{
+		
+	}
+	
+	//function that will direct message only one other client by nickname
+	public void tellOnePerson(String name)
 	{
 		
 	}
