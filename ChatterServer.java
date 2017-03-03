1088154ee9d112ebf98e910232289b98535d6bed
jmd338 <jmd338@georgetown.edu>
2017-03-03 16:24:34 -0500

    Outline

--- ChatterServer.java
+++ ChatterServer.java
@@ -12,7 +12,21 @@ public class ChatterServer
 		
 	}
 
-
+	
+	//subclass that waits for response from others for each client
+	public class ServerListens
+	{
+		
+	}
+	
+	//function to tell other ServerListens objects what each server is doing
+	//needs to be synchronized
+	//must go to every client
+	//opens a Writer to write to ClientListens
+	public void tellOthers()
+	{
+		
+	}
 
 }
 
